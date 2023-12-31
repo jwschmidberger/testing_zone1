f8bcc30339d70c5eee8f43e7bc2e44bde274ed27
jwschmidberger <jason.schmidberger@gmail.com>
2023-12-31 11:08:13 +0000

    starting 231231

---
+++ datacamp_deeplearning1.py
@@ -0,0 +1,20 @@
+'''
+Here I am working with a DataCamp Course that intruduces the concept of Deep Learning.
+'''
+
+#%%
+import numpy as np
+input_data = np.array([2, 3])
+weights = {'node_0': np.array([1, 1]),
+           'node_1': np.array([-1, 1]), 
+           'output': np.array([2, -1])}
+node_0_value = (input_data * weights['node_0']).sum()
+node_1_value = (input_data * weights['node_1']).sum()
+hidden_layer_values = np.array([node_0_value, node_1_value])
+print(hidden_layer_values)
+# %%
+hidden_layer_values.shape
+# %%
+output = (hidden_layer_values * weights['output']).sum()
+print(output)
+# %%
