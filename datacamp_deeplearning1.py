4cd542922e103349f89e5f8d0595f8b797b17676
jwschmidberger <jason.schmidberger@gmail.com>
2024-01-06 17:48:30 +0000

    240106 back propagation

--- datacamp_deeplearning1.py
+++ datacamp_deeplearning1.py
@@ -79,3 +79,21 @@ for row in input_data:
 results
 
 # %%
+# Adjusting the weights
+weights = np.array([1,2])
+input_data = np.array([3,4])
+target = 6
+learning_rate = 0.01
+preds = (weights * input_data).sum()
+error = preds - target
+print(error)
+
+#%%
+gradient = 2 * input_data * error
+weights_updated = weights - learning_rate * gradient
+preds_updated = (weights_updated * input_data).sum()
+error_updated = preds_updated - target
+print(error_updated)
+
+
+# %%
