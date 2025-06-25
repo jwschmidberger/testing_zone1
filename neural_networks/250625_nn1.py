681267fe475323317d4e4938610ceeffe1f42824
jwschmidberger <jason.schmidberger@gmail.com>
2025-06-25 22:28:59 +0100

    looking into a timeseries forcasting example

---
+++ neural_networks/250625_nn1.py
@@ -0,0 +1,138 @@
+#%%
+
+import numpy as np
+import pandas as pd
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
+from pytorch_lightning.loggers import TensorBoardLogger
+import torch
+
+
+#%%
+from pytorch_forecasting import Baseline, TemporalFusionTransformer, TimeSeriesDataSet
+from pytorch_forecasting.data import GroupNormalizer
+from pytorch_forecasting.metrics import SMAPE, PoissonLoss, QuantileLoss
+from pytorch_forecasting.models.temporal_fusion_transformer.tuning import optimize_hyperparameters
+
+
+
+# %%
+# This dataset is already included in pytorch forecasting library so import it using below commands:
+from pytorch_forecasting.data.examples import get_stallion_data
+
+
+# %%
+data = get_stallion_data()
+data
+# %%
+# add time index
+data["time_idx"] = data["date"].dt.year * 12 + data["date"].dt.month
+data["time_idx"] -= data["time_idx"].min()
+
+data
+# %%
+# add additional features
+data["month"] = data.date.dt.month.astype(str).astype("category")  # categories have be strings
+data["log_volume"] = np.log(data.volume + 1e-8)
+data["avg_volume_by_sku"] = data.groupby(["time_idx", "sku"], observed=True).volume.transform("mean")
+data["avg_volume_by_agency"] = data.groupby(["time_idx", "agency"], observed=True).volume.transform("mean")
+
+data
+
+
+# %%
+# create a list of special_days since we want to encode special days as one variable 
+special_days = [
+    "easter_day",
+    "good_friday",
+    "new_year",
+    "christmas",
+    "labor_day",
+    "independence_day",
+    "revolution_day_memorial",
+    "regional_games",
+    "fifa_u_17_world_cup",
+    "football_gold_cup",
+    "beer_capital",
+    "music_fest",
+]
+data[special_days] = data[special_days].apply(lambda x: x.map({0: "-", 1: x.name})).astype("category")
+data.sample(10, random_state=521)
+# %%
+data.describe()
+# %%
+data["time_idx"].max()
+
+# %%
+max_prediction_length = 6 # Number of months to predict = 6 (here)
+max_encoder_length = 24
+training_cutoff = data["time_idx"].max() - max_prediction_length
+
+training = TimeSeriesDataSet(
+    data[lambda x: x.time_idx <= training_cutoff],
+    time_idx="time_idx",
+    target="volume",
+    group_ids=["agency", "sku"],
+    min_encoder_length=max_encoder_length // 2,  # keep encoder length long (as it is in the validation set)
+    max_encoder_length=max_encoder_length,
+    min_prediction_length=1,
+    max_prediction_length=max_prediction_length,
+    static_categoricals=["agency", "sku"],
+    static_reals=["avg_population_2017", "avg_yearly_household_income_2017"],
+    time_varying_known_categoricals=["special_days", "month"],
+    variable_groups={"special_days": special_days},  # group of categorical variables can be treated as one variable
+    time_varying_known_reals=["time_idx", "price_regular", "discount_in_percent"],
+    time_varying_unknown_categoricals=[],
+    time_varying_unknown_reals=[
+        "volume",
+        "log_volume",
+        "industry_volume",
+        "soda_volume",
+        "avg_max_temp",
+        "avg_volume_by_agency",
+        "avg_volume_by_sku",
+    ],
+    target_normalizer=GroupNormalizer(
+        groups=["agency", "sku"], transformation="softplus"
+    ),  # use softplus and normalize by group
+    add_relative_time_idx=True,
+    add_target_scales=True,
+    add_encoder_length=True,
+)
+
+
+
+# %%
+# create validation set (predict=True) which means to predict the last max_prediction_length points in time for each series
+validation = TimeSeriesDataSet.from_dataset(training, data, predict=True, stop_randomization=True)
+
+
+
+# %%
+# create dataloaders for model
+batch_size = 128  # setting it between 32 to 128
+train_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=0)
+val_dataloader = validation.to_dataloader(train=False, batch_size=batch_size * 10, num_workers=0)
+
+
+
+# %%
+# calculate baseline mean absolute error, i.e. predict next value as the last available value from the history
+actuals = torch.cat([y for x, (y, weight) in iter(val_dataloader)])
+baseline_predictions = Baseline().predict(val_dataloader)
+(actuals - baseline_predictions).abs().mean().item()
+
+
+
+# %%
+# configure network and trainer
+pl.seed_everything(42)
+trainer = pl.Trainer(
+    accelerator="cpu",
+    devices=1,
+    # clipping gradients is a hyperparameter and important to prevent divergance
+    # of the gradient for recurrent neural networks
+    gradient_clip_val=0.1,
+)
+# %%
