a45e68b7fd5f5f92fcf482ee742ed95773623d35
jwschmidberger <jason.schmidberger@gmail.com>
2025-09-24 20:38:56 +0100

    added data and converted to df

--- neural_networks/250915_pytorch.py
+++ neural_networks/250915_pytorch.py
@@ -1,4 +1,25 @@
 #%%
+
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+#%%
 
+df = pd.read_csv('example.csv')
+
+
+df.columns = [
+    "Pregnancies",
+    "Glucose",
+    "BloodPressure",
+    "SkinThickness",
+    "Insulin",
+    "BMI",
+    "DiabetesPedigree",
+    "Age",
+    "Outcome",
+]
+df
+# %%
