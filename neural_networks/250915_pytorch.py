0158184186fa0afd4f3d47bb6b45b6803104e4de
jwschmidberger <jason.schmidberger@gmail.com>
2025-09-27 16:55:08 +0100

    tinkering with NN

--- neural_networks/250915_pytorch.py
+++ neural_networks/250915_pytorch.py
@@ -23,3 +23,17 @@ df.columns = [
 ]
 df
 # %%
+# load the dataset, split into input (X) and output (y) variables
+dataset = np.loadtxt('example.csv', delimiter=',')
+X = dataset[:,0:8]
+y = dataset[:,8]
+y.shape
+
+# %%
+X = torch.tensor(X, dtype=torch.float32)
+y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
+X
+
+
+# %%
+ 
